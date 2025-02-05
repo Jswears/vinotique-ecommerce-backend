5a4119f39d4d20abae15698b41df126a78950121
Jswears <ji.swearssalinas@gmail.com>
2025-02-05 10:03:22 +0100

    basic structure

--- src/functions/Wine/createWine/index.ts
+++ src/functions/Wine/createWine/index.ts
@@ -1,2 +1,28 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+
+export const handler = async (event: any) => {
+  const client = new DynamoDBClient({});
+  const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+  const { wine } = event;
+
+  const params = {
+    TableName: process.env.WINES_TABLE_NAME,
+    Item: wine,
+  };
+
+  try {
+    await ddbDocClient.send(new PutCommand(params));
+    return {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
+};
